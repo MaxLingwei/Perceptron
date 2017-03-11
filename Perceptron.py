c7432cfc18f2af0a107c50126cd838dbbbf3953b
MaxLingwei <U201213512@hust.edu.cn>
2017-03-11 20:34:58 +0800

    Finished version

--- Perceptron.py
+++ Perceptron.py
@@ -27,6 +27,7 @@ def train_perceptron(weight, learning_rate, train_set):
 
 			error_num += 1
 			iter_num += 1
+	return weight
 
 
 if __name__ == '__main__':
@@ -36,7 +37,12 @@ if __name__ == '__main__':
 	train_set_1 = []
 	train_set_2 = []
 	train_set_3 = []
-	weight_set = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+	weight_set = [[0.8, 1, -1.7], [1, 1, 1], [1, 1, 1]]
+
+
+	weight_test = [0.8, 1, -1.7]
+	test_set = [[[1, 1], 1], [[0, 1], 1], [[0, 0], 0], [[1, 0], 0]]
+
 	for i in range(0, len(origin_set)):
 		if origin_set[i][1] == 1:
 			train_set_1.append([origin_set[i][0], 1])
@@ -45,22 +51,26 @@ if __name__ == '__main__':
 
 	for i in range(0, len(origin_set)):
 		if origin_set[i][1] == 2:
-			train_set_1.append([origin_set[i][0], 1])
+			train_set_2.append([origin_set[i][0], 1])
 		else:
-			train_set_1.append([origin_set[i][0], 0])
+			train_set_2.append([origin_set[i][0], 0])
 
 	for i in range(0, len(origin_set)):
 		if origin_set[i][1] == 3:
-			train_set_1.append([origin_set[i][0], 1])
+			train_set_3.append([origin_set[i][0], 1])
 		else:
-			train_set_1.append([origin_set[i][0], 0])
+			train_set_3.append([origin_set[i][0], 0])
 
-	train_perceptron(weight_set[0], 0.2, train_set_1)
-	train_perceptron(weight_set[1], 0.2, train_set_2)
-	train_perceptron(weight_set[2], 0.2, train_set_3)
+	weight_set[0] = train_perceptron(weight_set[0], 1, train_set_1)
+	weight_set[1] = train_perceptron(weight_set[1], 1, train_set_2)
+	weight_set[2] = train_perceptron(weight_set[2], 1, train_set_3)
 
 	print "result"
-	for i in range(0, weight_set):
+	for i in range(0, len(weight_set)):
 		print weight_set[i]
+
+	'''weight_test = train_perceptron(weight_test, 1, test_set)
+
+	print weight_test'''
 
 		
